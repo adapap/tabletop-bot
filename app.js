1bd60072ce8bb7ad989c64a4dbfa9de9aa62a368
Adam Papenhausen <debomb55555@gmail.com>
2017-01-18 12:39:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
-const pokerBot = new Discord.CLient();
+const pokerBot = new Discord.Client();
 const prefix = "$";
 
 var numPlayers, turn = 9, dealt = 0;
 var nextMsg = "Use $deal to start the next round";
 
-bot.on("ready", () => {
+pokerBot.on("ready", () => {
   console.log("Poker Bot v0.4 loaded.");
 });
 
@@ -21,6 +21,21 @@ function shuffle(array) { //Shuffle Cards
   return array;
 }
 
+var Poker = {};
+  Poker.rankArray = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+  Poker.suitArray = [":clubs:",":diamonds:",":hearts:",":spades:"];
+  Poker.deck = [];
+  Poker.deckDisplay = [];
+  Poker.newDeck = function(players) {
+    numPlayers = players;
+    for(i=0; i<Poker.rankArray.length; i++) {
+      for(j=0; j<Poker.suitArray.length; j++) {
+      Poker.deck.push(Poker.rankArray[i] + Poker.suitArray[j]);
+      }
+    }
+  }
+
+pokerBot.on("message", message => {
 function code(lang, arg) {
   message.channel.sendCode(lang, arg);
 }
@@ -32,24 +47,8 @@ function embed(titleArg, desc) {
 function msg(arg) {
   message.channel.sendMessage(arg);
 }
-
-var Poker = {};
-  Poker.rankArray = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
-  Poker.suitArray = [":clubs:",":diamonds:",":hearts:",":spades:"];
-  Poker.deck = [];
-  Poker.deckDisplay = [];
-  Poker.newDeck = function(players) {
-    numPlayers = players;
-    for(i=0; i<rankArray.length; i++) {
-      for(j=0; j<suitArray.length; j++) {
-      Poker.deck.push(rankArray[i] + suitArray[j]);
-      }
-    }
-  }
-
-bot.on("message", message => {
   if (message.author.bot) return;
-  if (!message.content.startWith(prefix)) return;
+  if (!message.content.startsWith(prefix)) return;
 
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
@@ -57,6 +56,7 @@ bot.on("message", message => {
   let args = message.content.split(" ").slice(1);
   
   if (command === "poker") {
+    Poker.newDeck(2);
     msg("Shuffling...");
     shuffle(Poker.deck);
     for(i=0; i<Poker.deck.length; i++) {
@@ -67,14 +67,18 @@ bot.on("message", message => {
   }
   
   if (command === "deal") {
-    msg("Deal with it");
     switch(turn) {
       case 0:
         turn++;
+        for(i=0; i<numPlayers*2 - 1; i+=2) {
+            embed("Player " + (i/2 + 1) + "'s Cards",Poker.deckDisplay[i] + "   " + Poker.deckDisplay[i+1]);
+        }
       case 1:
         turn++;
       case 2:
+        turn++;
       case 3:
+        turn = 9;
       case 9:
         msg("Use $poker to shuffle and start again");
     }
@@ -89,3 +93,5 @@ bot.on("message", message => {
     code("fix","$help/$commands - Display this command list\n$deal - Deals cards to all players\n$draw - Draw a random card from the deck\n$poker - Resets the game by shuffling the deck");
   }
 });
+
+pokerBot.login("MjcxMDgyOTE0NTQyOTExNDkw.C2E3GQ.YPOqT8xC60_cCrxZCoQ-ZrJuPiE");
