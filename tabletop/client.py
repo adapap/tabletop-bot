a7a97a42c17f9c96216fc9d51e432016fb1d17ef
adapap <19696846+adapap@users.noreply.github.com>
2020-06-06 23:55:25 -0400

    Update

--- tabletop/client.py
+++ tabletop/client.py
@@ -1,17 +1,9 @@
 import discord
 from typing import Awaitable, Callable
 
-class Client:
-    """Interface for interacting with various service APIs.
-    
-    Current integrations supported:
-    Discord"""
+class TabletopClient:
+    """The client that handles running the different games supported by Tabletop."""
     error: Callable
     info: Callable
     message: Callable[[str], Awaitable[None]]
     warn: Callable
-
-class DiscordClient(Client, discord.Client):
-    """Client interface for Zoom chatbots."""
-    async def message(self, text: str) -> None:
-        pass    
